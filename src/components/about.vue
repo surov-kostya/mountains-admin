4eb3e18149dceb16e65531bf2b761c1e550f2ad3
Surov Konstantin <surov.qostya@yandex.ru>
2018-01-15 00:33:45 +0300

    Add data from json-file to about-page

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,10 +7,10 @@
           table.table
             tr.table__header
               th.table__title {{skillItem}}
-            tr.table__data(v-for="tech in skills")
-              td.table__technology {{tech}}
+            tr.table__data(v-for="tech in skills" v-if="checkSkillItem(skillItem) ===  tech.type")
+              td.table__technology {{tech.name}}
               td.table__input
-                input(type="text").table_percent
+                input(type="text" :value="tech.percents").table_percent
               td.table__sign %
       button(type="button") Сохранить
 </template>
@@ -25,6 +25,15 @@ export default {
     return{
       skillList: ['frontend', 'backend', 'workflow'],
       skills: []
+    }
+  },
+  methods:{
+    checkSkillItem(skillItem){
+      switch(skillItem) {
+        case 'frontend' : return 1;
+        case 'backend' : return 2;
+        case 'workflow' : return 3;
+      }
     }
   },
   created() {
