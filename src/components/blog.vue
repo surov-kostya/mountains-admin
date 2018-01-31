3d98ab52640e3a9d5cdfac66c4b57c79d6a38d62
Surov Konstantin <surov.qostya@yandex.ru>
2018-01-31 18:03:48 +0300

    Append form to blog layout

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   section.blog
     .blog__title Страница "Блог"
-    .blog__subtitle Добавить запись
-    input(type="text" placeholder="Название")
-    input(type="text" placeholder="Дата")
-    textarea(placeholder="Содержание")
-    button(type="button") Добавить
+    form.blog__form(method='POST', action='/article')
+      .blog__subtitle Добавить запись
+      input(type="text" name="title" placeholder="Название")
+      input(type="text" name="date" placeholder="Дата")
+      textarea(name="body" placeholder="Содержание")
+      button(type="submit") Добавить
 </template>
 <style lang="scss" src="styles/blog.scss" scoped>
 
